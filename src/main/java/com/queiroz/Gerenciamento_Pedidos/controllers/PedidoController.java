7d83f866643feebd5600e75ec69eb0fac0c0307f
ferqueiroz <ferqueiroz2205@gmail.com>
2026-03-18 20:32:08 -0300

    Finalizando as partes de requisições.

--- src/main/java/com/queiroz/Gerenciamento_Pedidos/controllers/PedidoController.java
+++ src/main/java/com/queiroz/Gerenciamento_Pedidos/controllers/PedidoController.java
@@ -1,13 +1,44 @@
 package com.queiroz.Gerenciamento_Pedidos.controllers;
 
+import com.queiroz.Gerenciamento_Pedidos.models.PedidoModel;
 import com.queiroz.Gerenciamento_Pedidos.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(name = "/pedidos")
+@RequestMapping("/pedidos")
 public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
+
+    @PostMapping
+    public ResponseEntity<PedidoModel> criar(PedidoModel pedidoModel) {
+        return ResponseEntity.status(201).body(pedidoService.criar(pedidoModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PedidoModel>> listar() {
+        return ResponseEntity.ok().body(pedidoService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoModel> buscar(@PathVariable Long id) {
+        Optional<PedidoModel> livro = pedidoService.buscar(id);
+
+        if (livro.isPresent())
+            return ResponseEntity.ok().body(livro.get());
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PedidoModel> deletar(@PathVariable Long id) {
+        pedidoService.excluir(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/queiroz/Gerenciamento_Pedidos/services/PedidoService.java
+++ src/main/java/com/queiroz/Gerenciamento_Pedidos/services/PedidoService.java
@@ -1,11 +1,32 @@
 package com.queiroz.Gerenciamento_Pedidos.services;
 
+import com.queiroz.Gerenciamento_Pedidos.models.PedidoModel;
 import com.queiroz.Gerenciamento_Pedidos.repositories.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
+
+    public PedidoModel criar(PedidoModel pedidoModel) {
+        return pedidoRepository.save(pedidoModel);
+    }
+
+    public List<PedidoModel> listar() {
+        return pedidoRepository.findAll();
+    }
+
+    public Optional<PedidoModel> buscar(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public void excluir(Long id) {
+        pedidoRepository.deleteById(id);
+    }
 }
